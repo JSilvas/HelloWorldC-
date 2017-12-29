8a485948b273f2e7e3e6a2392837cc7e46a2ac7b
Jay Silvas <jayinlava@gmail.com>
2017-12-28 18:47:14 -0800

    Add project files.

---
+++ HelloWorldC++/HelloWorldDriver.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+//#include "popcorn.cpp"
+
+int main() {                                //creates method *main*
+	std::string greeting = "Hello World!";
+	std::cout << greeting;        //use namespace *std::* to look for cout and string "Hello World!"
+	return 0;
+}
